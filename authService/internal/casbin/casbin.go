d08cf6896e619b7046a797c32b8d6ae7f42cc36e
chencheng8888 <2963801553@qq.com>
2024-11-17 10:00:12 +0800

    feat: casbin相关功能的完成
    1、完成了casbin的policy的编写
    2、完成了为用户分配权限以及验证权限

---
+++ authService/internal/casbin/casbin.go
@@ -0,0 +1,168 @@
+package casbin
+
+import (
+	"errors"
+	"fmt"
+	"github.com/casbin/casbin/v2"
+	gormadapter "github.com/casbin/gorm-adapter/v3"
+	"github.com/chencheng8888/tiktok_e-commence/authService/internal/conf"
+	"github.com/chencheng8888/tiktok_e-commence/authService/internal/model"
+	"github.com/google/wire"
+)
+
+var (
+	ErrInvalidSubject = errors.New("invalid subject")
+	ErrAssignRole     = errors.New("assign role failed")
+	ErrSavePolicy     = errors.New("save policy failed")
+	ErrCasBinEnforce  = errors.New("casbin enforce policy failed")
+	ErrInvalidAct     = errors.New("invalid act")
+)
+
+// ProviderSet is casbin providers.
+var ProviderSet = wire.NewSet()
+
+type CheckActer interface {
+	CheckActExist(act string) bool
+}
+
+type AuthCase struct {
+	c *casbin.Enforcer
+}
+
+func NewAuthCase(cf *conf.Data) *AuthCase {
+	// casbin使用gorm作为适配器
+	// 使用 cf.Database.Driver 作为数据库,并且打开自动建表(若未存在)
+	a, err := gormadapter.NewAdapter(cf.Casbin.Driver, cf.Casbin.Source, true)
+	if err != nil {
+		panic(err)
+	}
+	e, err := casbin.NewEnforcer("./model.conf", a)
+	if err != nil {
+		panic(err)
+	}
+	err = initPolicy(e)
+	if err != nil {
+		panic(err)
+	}
+	return &AuthCase{c: e}
+}
+func initPolicy(e *casbin.Enforcer) error {
+	// 注意: 这个AddPolicies如果一旦遇到已经添加过的policy,它是直接返回added=true,不执行后面的policy,也就是说
+	// 一旦你在数据库中删去了老的规则,在这里添加了新的规则,实际上是不会执行的,如果是加在开头,可能有用
+	_, err := e.AddPolicies(
+		[][]string{
+			// *代表所有
+			{model.Admin, "*", "*", model.ALLOW},
+
+			{model.BlackLister, "*", "*", model.DENY},
+
+			//{model.Traveler,model.U.String(),model.U.Create(),model.ALLOW},
+			//{model.Traveler,model.U.String(),model.U.Update(),model.DENY},
+			//{model.Traveler,model.U.String(),model.U.Login(),model.ALLOW},
+			//{model.Traveler,model.U.String(),model.U.Logout(),model.DENY},
+			//{model.Traveler,model.U.String(),model.U.Delete(),model.DENY},
+			//{model.Traveler,model.U.String(),model.U.GetInfo(),model.DENY},
+
+			//由于在model.conf中
+			//[policy_effect]
+			//e = some(where (p.eft == allow))
+			//由于游客大部分的操作都是不被允许的，为了简洁起见，所以就先否定所有，在从中选出其中可以执行的操作
+			{model.Traveler, "*", "*", model.DENY},
+			{model.Traveler, model.U.String(), model.U.Create(), model.ALLOW},
+			{model.Traveler, model.U.String(), model.U.Login(), model.ALLOW},
+			{model.Traveler, model.I.String(), model.I.GetInfo(), model.ALLOW},
+
+			{model.NormalUser, model.U.String(), model.U.Create(), model.ALLOW},
+			{model.NormalUser, model.U.String(), model.U.Update(), model.ALLOW},
+			{model.NormalUser, model.U.String(), model.U.Login(), model.ALLOW},
+			{model.NormalUser, model.U.String(), model.U.Logout(), model.ALLOW},
+			{model.NormalUser, model.U.String(), model.U.Delete(), model.ALLOW},
+			{model.NormalUser, model.U.String(), model.U.GetInfo(), model.ALLOW},
+			{model.NormalUser, model.I.String(), model.I.Create(), model.DENY},
+			{model.NormalUser, model.I.String(), model.I.GetInfo(), model.ALLOW},
+			{model.NormalUser, model.I.String(), model.I.UpdateInfo(), model.DENY},
+			{model.NormalUser, model.I.String(), model.I.Delete(), model.DENY},
+			{model.NormalUser, model.S.String(), "*", model.ALLOW},
+			{model.NormalUser, model.O.String(), "*", model.ALLOW},
+			{model.NormalUser, model.P.String(), "*", model.ALLOW},
+
+			{model.Merchant, "*", "*", model.DENY},
+			{model.Merchant, model.U.String(), "*", model.ALLOW},
+			{model.Merchant, model.I.String(), "*", model.ALLOW},
+		})
+	if err != nil {
+		return err
+	}
+	err = e.SavePolicy()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// AssignAuthority 为用户分配权限
+// 成功返回true,失败返回false
+func (a *AuthCase) AssignAuthority(userID int32, role string) error {
+	//判断角色是否合法
+	switch role {
+	case model.Admin, model.Traveler, model.NormalUser, model.Merchant, model.BlackLister:
+	default:
+		return ErrInvalidSubject
+	}
+	id := fmt.Sprintf("%d", userID)
+	// 为用户分配角色
+	_, err := a.c.AddGroupingPolicy(id, role)
+	if err != nil {
+		return ErrAssignRole
+	}
+	//保存
+	if err := a.c.SavePolicy(); err != nil {
+		return ErrSavePolicy
+	}
+	return nil
+}
+
+// VerifyAuthority 验证 userID对于obj资源是否可以执行act操作
+// 如果执行成功,返回验证结果
+// 如果操作不合法或者obj不合法,就会返回error("invalid act")
+// 如果执行失败就返回error("casbin enforce policy failed")
+// obj及对应的act在下面
+// user : "CREATE","LOGIN","LOGOUT","DELETE","UPDATE","GET"
+// shopping_cart : "CREATE","CLEAR","GET"
+// pay : "PAY","CANCEL"
+// order : "CREATE","UPDATE","SETTLE"
+// item : "CREATE","DELETE","UPDATE","GET"
+func (a *AuthCase) VerifyAuthority(userID int32, obj, act string) (bool, error) {
+	if !checkAct(obj, act) {
+		return false, ErrInvalidAct
+	}
+
+	id := fmt.Sprintf("%d", userID)
+	ok, err := a.c.Enforce(id, obj, act)
+	if err != nil {
+		return false, ErrCasBinEnforce
+	}
+	return ok, nil
+}
+
+// 检查对obj是否有act操作
+// 使用映射来减少 switch 语句
+func checkAct(obj, act string) bool {
+	// 定义一个映射，直接将 obj 映射到具体的类型
+	acters := map[string]CheckActer{
+		model.U.String(): model.User{},
+		model.I.String(): model.Item{},
+		model.O.String(): model.Order{},
+		model.P.String(): model.PayService{},
+		model.S.String(): model.ShoppingCart{},
+	}
+
+	// 获取对应类型的实例
+	checker, exists := acters[obj]
+	if !exists {
+		return false
+	}
+
+	// 调用 CheckActExist 方法
+	return checker.CheckActExist(act)
+}
